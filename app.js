f5d5b3db6cf6ad1370fdcacdc913a89f0cb83e29
Yan.Palyn <Kvinto1986@gmail.com>
2019-07-19 12:04:52 +0300

    add websocketServer

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const http = require("http");
-
-const users = require('./routes/usersRoute');
-const customers = require('./routes/customersRoute');
-const texts = require('./routes/textRoute');
-const uploads = require('./routes/uploadRoute');
-const messages = require('./routes/messageRoute');
-const translates = require('./routes/translateRoute');
 
 const app = express();
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cors());
 
-app.use('/api/users', users);
-app.use('/api/customers', customers);
-app.use('/api/texts', texts);
-app.use('/api/uploads', uploads);
-app.use('/api/messages', messages);
-app.use('/api/translates', translates)
-
-const PORT = process.env.PORT || 5000;
-
-const server = app.listen(4000);
+const server = app.listen(5000);
 const io = require('socket.io').listen(server);
 
 io.on("connection", socket => {
@@ -54,6 +37,3 @@ io.on("connection", socket => {
     })
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}`);
-});
--- routes/logic/translates/compareResponce.js
+++
@@ -1,7 +0,0 @@
-
-const compareResponce = (req, res, next) => {
-    const {translate, customerData} = req;
-    res.json({translate, customerData})
-};
-
-module.exports = compareResponce;

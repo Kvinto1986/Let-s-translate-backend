54f794cff6fb75e028d32ccecaaacdee91e23519
Yan.Palyn <Kvinto1986@gmail.com>
2019-07-19 12:08:01 +0300

    add websocketServer

--- app.js
+++ app.js
@@ -24,36 +24,6 @@ app.use('/api/translates', translates)
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(4000);
-const io = require('socket.io').listen(server);
-
-io.on("connection", socket => {
-
-    socket.on('newTextData', data => {
-        io.sockets.emit("newTextAlert", data);
-    })
-    
-    socket.on('newTranslateStatusData', data => {
-        io.sockets.emit("newTranslateStatusAlert", data);
-    })
-
-    socket.on('newUnReadMessage', data => {
-        io.sockets.emit("spawnMessage", data);
-    })
-
-    socket.on('typingEcho', data => {
-        io.sockets.emit("typing", data);
-    })
-
-    socket.on('stopTypingEcho', data => {
-        io.sockets.emit("stopTyping", data);
-    })
-
-    socket.on('didMountUnreadMessageCountDiscard', data => {
-        io.sockets.emit("unreadMessageCountDiscard", data);
-    })
-})
-
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
 });

d1f92588657fd4b0540c8ab11ab674b4518f2d0d
Yan.Palyn <Kvinto1986@gmail.com>
2019-07-22 12:34:02 +0300

    add websocketServer

--- routes/customersRoute.js
+++ routes/customersRoute.js
@@ -22,83 +22,81 @@ router.post('/registration', function (req, res) {
 
     Customer.findOne({where: {[Op.or]: [{email: req.body.email}, {creditCard: req.body.creditCard}]}})
         .then(customer => {
-                if (customer) {
-                    return res.status(400).json({
-                        email: 'Email or credit card already exists'
-                    });
-                } else {
+            if (customer) {
+                return res.status(400).json({
+                    email: 'Email or credit card already exists'
+                });
+            } else {
 
-                    const transporter = nodemailer.createTransport({
-                        service: 'gmail',
-                        auth: {
-                            user: 'managerjohnsnow@gmail.com',
-                            pass: 'John1234567890Snow'
-                        }
-                    });
+                const transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        user: 'managerjohnsnow@gmail.com',
+                        pass: 'John1234567890Snow'
+                    }
+                });
 
-                    let userData = req.body.creditCard + req.body.email;
+                let userData = req.body.creditCard + req.body.email;
 
-                    bcrypt.genSalt(10, (err, salt) => {
-                        if (err) console.error('There was an error', err);
-                        else {
-                            bcrypt.hash(userData, salt, (err, hash) => {
-                                if (err) console.error('There was an error', err);
-                                else {
-                                    userData = hash;
-                                    const mailOptions = {
-                                        from: 'managerJohnSnow@gmail.com',
-                                        to: req.body.email,
-                                        subject: 'You have successfully registered with the Let\'s translate!',
-                                        html: `<h1>Congratulations! You have successfully registered in our system, success in your work!</h1> <a href='https://letstranslate-app.herokuapp.com/confirm/${userData}'>Follow the link to confirm profile creation.</a>`
-                                    };
-
-                                    transporter.sendMail(mailOptions, function (error, info) {
-                                        if (error) {
-                                            res.json(error);
-                                        } else {
-                                            res.json('Email sent: ' + info.response);
-                                        }
-                                    });
-                                }
-                            });
-                        }
-                    }).then(() => {
-
-                        const newCustomer = new Customer({
-                            name: req.body.name,
-                            email: req.body.email,
-                            password: req.body.password,
-                            verify: false,
-                            texts: [],
-                            creditCard: req.body.creditCard,
-                            role: req.body.role,
-                            languages: req.body.languages,
-                            date: Date.now()
-                        });
-
-                        bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.genSalt(10, (err, salt) => {
+                    if (err) console.error('There was an error', err);
+                    else {
+                        bcrypt.hash(userData, salt, (err, hash) => {
                             if (err) console.error('There was an error', err);
                             else {
-                                bcrypt.hash(newCustomer.password, salt, (err, hash) => {
-                                    if (err) console.error('There was an error', err);
-                                    else {
-                                        newCustomer.password = hash;
-                                        newCustomer
-                                            .save()
-                                            .then(user => {
-                                                res.json(user)
-                                            });
+                                userData = hash;
+                                const mailOptions = {
+                                    from: 'managerJohnSnow@gmail.com',
+                                    to: req.body.email,
+                                    subject: 'You have successfully registered with the Let\'s translate!',
+                                    html: `<h1>Congratulations! You have successfully registered in our system, success in your work!</h1> <a href='https://letstranslate-app.herokuapp.com/confirm/${userData}'>Follow the link to confirm profile creation.</a>`
+                                };
+
+                                transporter.sendMail(mailOptions, function (error, info) {
+                                    if (error) {
+                                        console.log(error);
+                                    } else {
+                                        console.log('Email sent: ' + info.response);
                                     }
                                 });
                             }
                         });
+                    }
+                });
 
-                        Customer.create(newCustomer);
-                        res.json(newCustomer)
-                    })
-                }
+                const newCustomer = new Customer({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password,
+                    verify: false,
+                    texts: [],
+                    creditCard: req.body.creditCard,
+                    role: req.body.role,
+                    languages: req.body.languages,
+                    date: Date.now()
+                });
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    if (err) console.error('There was an error', err);
+                    else {
+                        bcrypt.hash(newCustomer.password, salt, (err, hash) => {
+                            if (err) console.error('There was an error', err);
+                            else {
+                                newCustomer.password = hash;
+                                newCustomer
+                                    .save()
+                                    .then(user => {
+                                        res.json(user)
+                                    });
+                            }
+                        });
+                    }
+                });
+
+                Customer.create(newCustomer);
+                res.json(newCustomer)
             }
-        );
+        });
 });
 
 router.post('/login', (req, res) => {
@@ -230,6 +228,8 @@ router.post('/restorePassword', (req, res) => {
                     restoreCustomer: 'Customer not found'
                 });
             } else {
+
+
                 const transporter = nodemailer.createTransport({
                     service: 'gmail',
                     auth: {
@@ -247,12 +247,12 @@ router.post('/restorePassword', (req, res) => {
 
                 transporter.sendMail(mailOptions, function (error, info) {
                     if (error) {
-                        res.json({email:'Email not'});
+                        console.log(error);
                     } else {
-                        res.json({email:'Email sent'});
+                        console.log('Email sent: ' + info.response);
                     }
                 });
-
+                res.json(customer);
 
             }
         });
@@ -260,10 +260,10 @@ router.post('/restorePassword', (req, res) => {
 
 router.post('/newPassword', (req, res) => {
     Customer.findOne({where: {password: req.body.password}})
-        .then(customer => {
-            if (customer) {
-                res.json(customer)
-            } else res.status(400).json({
+        .then(customer =>{
+            if(customer){
+                res.json(customer)}
+            else res.status(400).json({
                 email: 'No'
             });
         })
@@ -271,15 +271,19 @@ router.post('/newPassword', (req, res) => {
 
 router.post('/editPassword', (req, res) => {
 
-    if (req.body.password !== req.body.password_confirm) {
+    if (req.body.password!==req.body.password_confirm) {
         res.status(400).json({
             password: 'Passwords do not match'
         });
-    } else if (req.body.password.length < 6) {
+    }
+
+    else if (req.body.password.length<6) {
         res.status(400).json({
             password: 'Password must have more then 6 chars'
         });
-    } else {
+    }
+
+    else {
         Customer.findOne({where: {id: req.body.id}})
             .then(customer => {
                 bcrypt.genSalt(10, (err, salt) => {

879232d8db775d5260d856d2f6a152c60522951d
Yan.Palyn <Kvinto1986@gmail.com>
2019-07-25 13:28:16 +0300

    fix edit text

--- routes/logic/text/textUpdate.js
+++ routes/logic/text/textUpdate.js
@@ -2,30 +2,35 @@ const Text = require('../../../models/TextModel');
 
 const textUpdate = (req, res) => {
 
+    if (req.body.originalLanguage === req.body.translationLanguage) {
+        return res.status(400).json({
+            language: 'Original and translation languages must be different'
+        });
+    }
     Text.findOne({
         where: {
             id: req.body.translateID
         }
     })
-    .then(result => {
-        result.originalLanguage = req.body.originalLanguage;
-        result.translationLanguage = req.body.translationLanguage;
-        result.extraReview = req.body.extraReview;
-        result.translationSpeed = req.body.translationSpeed;
-        result.charsCount = req.body.charsCount;
-        result.tags = req.body.tags;
+        .then(result => {
+            result.originalLanguage = req.body.originalLanguage;
+            result.translationLanguage = req.body.translationLanguage;
+            result.extraReview = req.body.extraReview;
+            result.translationSpeed = req.body.translationSpeed;
+            result.charsCount = req.body.charsCount;
+            result.tags = req.body.tags;
 
-        if (req.body.fileName) {
-            result.fileName = req.body.fileName;
-        }
+            if (req.body.fileName) {
+                result.fileName = req.body.fileName;
+            }
 
-        if (req.body.fileUrl) {
-            result.fileUrl = req.body.fileUrl;
-        }
+            if (req.body.fileUrl) {
+                result.fileUrl = req.body.fileUrl;
+            }
 
-        result.save()
-    })
-    .then(() => res.json(req.body))
+            result.save()
+        })
+        .then(() => res.json(req.body))
 }
 
 module.exports = textUpdate

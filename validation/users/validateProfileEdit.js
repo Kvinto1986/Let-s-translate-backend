265e25f385acddceecff4a2d601d34c5b6a77031
Maksim Yepifanau <Maksim.Yepifanau@itechart-group.com>
2019-08-01 14:41:58 +0300

    validation and manage fix

--- validation/users/validateProfileEdit.js
+++ validation/users/validateProfileEdit.js
@@ -4,6 +4,18 @@ const isEmpty = require('../is-empty');
 module.exports = function validateProfileEdit(data) {
     let errors = {};
     
+    if (data.name.length > 0 && !(/^[a-zA-Z][a-zA-Z\s]+$/g.test(data.name))) {
+        errors.name = 'Use only Latin symbols in name.'
+    }
+
+    if(data.name.length > 0 && !(/^[a-zA-Z]+$/g.test(data.name.charAt(0)))) {
+        errors.name = 'Name should be started from any Latin symbol.'
+    }
+
+    if(data.name.length > 0 && data.name.length < 2) {
+        errors.name = 'User name needs to contains at list 2 symbols'
+    }
+    
     if(!Validator.isEmail(data.email) && data.email.length > 0) {
         errors.email = 'Email is invalid';
     }
@@ -19,11 +31,6 @@ module.exports = function validateProfileEdit(data) {
     if(Validator.isEmpty(data.passwordCur)) {
         errors.password_cur = 'Current password is required';
     }
-    
-    // Object.values(data)
-    // .filter(value => value === '') {
-
-    // }
 
     return {
         errors,
